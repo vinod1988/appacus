90fc04d0c0ed1af07046e31576ef1984464721ba
Tom Beynon <tom@tombeynon.co.uk>
2012-12-28 20:15:20 +0000

    cleaning up

--- Appacus - Tom/ViewController.m
+++ Appacus - Tom/ViewController.m
@@ -18,28 +18,42 @@
 @synthesize question3;
 @synthesize question4;
 @synthesize question5;
+@synthesize answer1;
+@synthesize answer2;
+@synthesize answer3;
+@synthesize answer4;
+@synthesize answer5;
 
+int level = 2;
 
 - (IBAction)initialiseAction:(id)sender{
-    NSMutableArray *chosenNumbers = [NSMutableArray array];
-    
+    NSMutableArray *chosenQuestions = [NSMutableArray array];
+    NSMutableArray *answerPositions = [NSMutableArray array];
+
     for (int i=0 ;i<5 ;i++) {
-        int num = 0;
-        while (num == 0 || [chosenNumbers containsObject:[NSNumber numberWithInt:num]]) {
-            num = arc4random() % 11;
-            num++;
+        // Generate random question and add to chosenQuestions array
+        int question = 0;
+        while (question == 0 || [chosenQuestions containsObject:[NSNumber numberWithInt:question]]) {
+            question = arc4random() % 11;
+            question++;
         }
-        [chosenNumbers addObject:[NSNumber numberWithInt:num]];
-        NSLog(@"%i",num);
-        NSLog(@"2 x %@ = ", [chosenNumbers objectAtIndex:i]);
+        [chosenQuestions addObject:[NSNumber numberWithInt:question]];
+        
+        // Generate answers from random questions 
+        int answerPosition = 0;
+        while (answerPosition == 0 || [answerPositions containsObject:[NSNumber numberWithInt:answerPosition]]) {
+            answerPosition = arc4random() % 4;
+            answerPosition++;
+        }
+        [answerPositions addObject:[NSNumber numberWithInt:answerPosition]];
     }
-    //NSLog(@"2 x %@ = ", [chosenNumbers objectAtIndex:2]);
-    //[question1 setText:(@"2 x %@ =",[chosenNumbers objectAtIndex:1])];
-    [question1 setText:[NSString stringWithFormat:@"2 x %@ = ", [chosenNumbers objectAtIndex:0]]];
-    [question2 setText:[NSString stringWithFormat:@"2 x %@ = ", [chosenNumbers objectAtIndex:1]]];
-    [question3 setText:[NSString stringWithFormat:@"2 x %@ = ", [chosenNumbers objectAtIndex:2]]];
-    [question4 setText:[NSString stringWithFormat:@"2 x %@ = ", [chosenNumbers objectAtIndex:3]]];
-    [question5 setText:[NSString stringWithFormat:@"2 x %@ = ", [chosenNumbers objectAtIndex:4]]];
+    
+    //input the random questions from the chosenQuestions Array
+    [question1 setText:[NSString stringWithFormat:@"%i x %@ = ",level, [chosenQuestions objectAtIndex:0]]];
+    [question2 setText:[NSString stringWithFormat:@"%i x %@ = ",level, [chosenQuestions objectAtIndex:1]]];
+    [question3 setText:[NSString stringWithFormat:@"%i x %@ = ",level, [chosenQuestions objectAtIndex:2]]];
+    [question4 setText:[NSString stringWithFormat:@"%i x %@ = ",level, [chosenQuestions objectAtIndex:3]]];
+    [question5 setText:[NSString stringWithFormat:@"%i x %@ = ",level, [chosenQuestions objectAtIndex:4]]];
 }
 
 - (void)viewDidLoad
