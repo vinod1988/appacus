c2a69fc5386ee9738132c1521025e04eb7b1632f
Matthew Nieuwenhuys <Matty@Matthew-Nieuwenhuyss-MacBook-Pro.local>
2012-12-28 18:48:20 +0000

    Initial commit

---
+++ Appacus - Tom/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Appacus - Tom
+//
+//  Created by Matthew Nieuwenhuys on 28/12/2012.
+//  Copyright (c) 2012 Matthew Nieuwenhuys. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
