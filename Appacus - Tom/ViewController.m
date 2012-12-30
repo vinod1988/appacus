d4b420ba7bcab7f1f07516c8ce325a428f2befad
Tom Beynon <tom@tombeynon.co.uk>
2012-12-30 19:56:30 +0000

    STABLE Game now loads automatically. Removed Start Game button Check now works as a reset button once user has completed the game. Will continue allowing them to check their answers until they have answered all Fully implemented returning target to answer by clicking with nothing selected Major refactoring. Game logic is now handled in GameController (game id in viewController), and view logic in the View controller. Split out of regularly used functionality into separate methods.

--- Appacus - Tom/ViewController.m
+++ Appacus - Tom/ViewController.m
@@ -16,159 +16,199 @@
 @synthesize questionLabels;
 @synthesize targetButtons;
 @synthesize answerButtons;
-@synthesize questions;
-@synthesize userAnswers;
 
-- (IBAction)initialiseAction:(id)sender
+// Run once scene (viewController) has loaded
+- (void)viewDidLoad
 {
-    // Order questionLabels array by Y position
-    self.questionLabels = [self.questionLabels sortedArrayUsingComparator:^NSComparisonResult(id label1, id label2) {
-        if ([label1 frame].origin.y < [label2 frame].origin.y) return NSOrderedAscending;
-        else if ([label1 frame].origin.y > [label2 frame].origin.y) return NSOrderedDescending;
-        else return NSOrderedSame;
-    }];
+    [super viewDidLoad];
     
+    [self initElementArrays];
     
-    // Order targetButtons array by Y position
-    self.targetButtons = [self.targetButtons sortedArrayUsingComparator:^NSComparisonResult(id label1, id label2) {
-        if ([label1 frame].origin.y < [label2 frame].origin.y) return NSOrderedAscending;
-        else if ([label1 frame].origin.y > [label2 frame].origin.y) return NSOrderedDescending;
-        else return NSOrderedSame;
-    }];
+    // Initialise GameController object if not initialized
+    if(![game isKindOfClass:[GameController class]]){
+        // Reset the game
+        [self resetGame];
+    }
+}
+
+// 'Hold' the answer if it hasn't been placed already
+- (IBAction)touchAnswer:(id)sender
+{
+    // Only trigger an action if game is in progress
+    if(![game complete]){
+        UIButton *button = (UIButton *)sender;
+        int position = [answerButtons indexOfObject:button]; // Get button position
+        id answer = [[game answers] objectAtIndex:position]; // Use button position as the index of answer in answers array
+        
+        // Has this answer been placed already?
+        if(![[game userAnswers] containsObject:answer]){
+            // Highlight button colour
+            [button setTitleColor:[UIColor colorWithWhite:1 alpha:1] forState:UIControlStateNormal];
+            // Reset currently held answer to it's original position if available
+            if([game heldAnswer] > 0){
+                int previousIndex = [[game answers] indexOfObject:[NSNumber numberWithInt:[game heldAnswer]]];
+                id previousButton = [answerButtons objectAtIndex:previousIndex];
+                [previousButton setTitleColor:[UIColor colorWithWhite:0 alpha:1] forState:UIControlStateNormal];
+            }
+            // This answer has been 'picked up'
+            [game setHeldAnswer:[answer intValue]];
+        }
+    }
+}
+
+// Place the answer on the target if target is available. Otherwise, reset the question current in target
+- (IBAction)touchTarget:(id)sender
+{
+    // Only trigger an action if game is in progress
+    if(![game complete]){
+        UIButton *targetButton = (UIButton *)sender;
+        int position = [targetButtons indexOfObject:targetButton];
+        int answer = [game heldAnswer];
+        // Check if there is an answer in-hand
+        if(answer > 0){
+            int answerIndex = [[game answers] indexOfObject:[NSNumber numberWithInt:answer]];
+            id answerButton = [answerButtons objectAtIndex:answerIndex];
+            // Check if this target has already been used
+            if([[game userAnswers] objectAtIndex:position] == (id)[NSNull null]){
+                // Set that targetButton to the answer and remove the answerButton
+                [self setTarget:targetButton withAnswer:answerButton]; 
+                [game setHeldAnswer:0]; // Reset held answer
+            }
+        }else{
+            // User isn't dropping anything on the target. 
+            // Reset buttons if possible
+            [self replaceTarget:targetButton];
+            
+        }
+    }
+}
+
+- (IBAction)checkAnswers:(id)sender{
     
-    // Order answerButtons array by Y position
-    self.answerButtons = [self.answerButtons sortedArrayUsingComparator:^NSComparisonResult(id label1, id label2) {
-        if ([label1 frame].origin.y < [label2 frame].origin.y) return NSOrderedAscending;
-        else if ([label1 frame].origin.y > [label2 frame].origin.y) return NSOrderedDescending;
-        else return NSOrderedSame;
-    }];
+    UIButton *checkButton = (UIButton *)sender;
     
-    // Initialise GameController object
+    // Only trigger an action if game is in progress
+    if(![game complete]){
+    
+        [game calculateScore];
+        
+        for(int i=0;i<[game numQuestions];i++) {
+            id targetButton = [targetButtons objectAtIndex:i];
+            id userAnswer = [[game userAnswers] objectAtIndex:i];
+            id question = [[game questions] objectAtIndex:i];
+            if(userAnswer != (id)[NSNull null] && [game calculateAnswer:[question intValue]] == [userAnswer intValue]){
+                [targetButton setTitleColor:[UIColor greenColor] forState:UIControlStateNormal];
+            }else{
+                [targetButton setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
+            }
+        }
+        
+        [game notifyScore];
+        
+        // Check if user has answered all questions. If so, allow user to reset game.
+        // This could be used to send the user elsewhere, such as into the next level
+        if([game numAnswered] == [game numQuestions]){
+            [game setComplete:true]; // Tell game that it's now complete
+            [checkButton setTitle:[NSString stringWithFormat:@"Reset game"] forState:UIControlStateNormal]; // Switch checkButton title to 'Reset game'. checkAnswers handles the rest
+        }
+    }else{
+        // Reset view and game
+        [self resetGame];
+        [checkButton setTitle:[NSString stringWithFormat:@"Check"] forState:UIControlStateNormal]; // Switch checkButton title back to 'Check'
+    }
+}
+
+// Set the targetButton to answerButton and 'remove' answerButton
+- (void)setTarget:(id)targetButton withAnswer:(id)answerButton{
+    int targetPosition = [targetButtons indexOfObject:targetButton];
+    int answerPosition = [answerButtons indexOfObject:answerButton];
+    // Find the answer from the answerPosition
+    int value = [[[game answers] objectAtIndex:answerPosition] intValue];
+    [[game userAnswers] replaceObjectAtIndex:targetPosition withObject:[NSNumber numberWithInt:value]];
+    // Remove answer from answerButton
+    [answerButton setTitleColor:[UIColor colorWithWhite:0 alpha:1] forState:UIControlStateNormal];
+    [answerButton setTitle:[NSString stringWithFormat:@""]  forState: UIControlStateNormal];
+    [answerButton setBackgroundImage:nil forState: UIControlStateNormal];
+    // Appear to move the answer onto the target
+    [targetButton setTitle:[NSString stringWithFormat:@"%i", value]  forState: UIControlStateNormal];
+    [targetButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    [targetButton setBackgroundImage:[UIImage imageNamed:@"AnswerBox.png"] forState: UIControlStateNormal];
+}
+
+// Replace the questionButton currently 'on' targetButton.
+- (void)replaceTarget:(id)targetButton{
+    int targetindex = [targetButtons indexOfObject:targetButton];
+    id userAnswer = [[game userAnswers] objectAtIndex:targetindex];
+    if(userAnswer != [NSNull null]){
+        // Find the original position of the answer currently 'on' the targetButton
+        int originalPosition = [[game answers] indexOfObject:userAnswer];
+        id originalButton = [answerButtons objectAtIndex:originalPosition];
+        [[game userAnswers] replaceObjectAtIndex:targetindex withObject:[NSNull null]]; // Remove from the userAnswers array
+        // Reset answer to it's original button
+        [originalButton setTitle:[NSString stringWithFormat:@"%i", [userAnswer intValue]]  forState: UIControlStateNormal];
+        [originalButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+        [originalButton setBackgroundImage:[UIImage imageNamed:@"AnswerBox.png"] forState: UIControlStateNormal];
+        // Reset targetButton to empty style
+        [targetButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+        [targetButton setTitle:[NSString stringWithFormat:@""]  forState: UIControlStateNormal];
+        [targetButton setBackgroundImage:[UIImage imageNamed:@"TransparentAnswerBox.png"] forState: UIControlStateNormal];
+    }
+}
+
+// (Re)set the game and populate the game elements with corresponding answers and questions
+- (void)resetGame{
     game = [[GameController alloc] init];
     
-    // Set the level
     [game setLevel:2];
-    
-    // Set the number of questions. This could be calculated from the labels.. but this feels safer
     [game setNumQuestions:5];
     
     // Reset the game
-    [game resetGame];
+    [game repopulateGame];
     
     // Set the labels from the answer and question arrays
     for(int i=0;i<[game numQuestions];i++) {
         // Set questionLabel text
         id questionLabel = [questionLabels objectAtIndex:i];
         id question = [[game questions] objectAtIndex:i];
-        [questionLabel setText:[NSString stringWithFormat:@"%i x %i = ",level, [question intValue]]];
+        [questionLabel setText:[NSString stringWithFormat:@"%i x %i = ",[game level], [question intValue]]];
         
-        // Find the answer element and update it's text
-        id answerLabel = [answerButtons objectAtIndex:i];
-        id answer = [[game answers] objectAtIndex:i];
-        [answerLabel setTitle:[NSString stringWithFormat:@"%i", [answer intValue]] forState: UIControlStateNormal];
-    }
-
-}
-
-- (IBAction)touchAnswer:(id)sender
-{
-    UIButton *button = (UIButton *)sender;
-    // Get button position
-    int position = [answerButtons indexOfObject:button];
-    // Use button position as the index of answer in answers array
-    id answer = [[game answers] objectAtIndex:position];
-    
-    // Has this answer been placed already?
-    if(![[game userAnswers] containsObject:answer]){
-        // Highlight button colour
-        [button setTitleColor:[UIColor colorWithWhite:1 alpha:1] forState:UIControlStateNormal];
-        // Reset currently held answer to it's original position if available
-        if([game heldAnswer] > 0){
-            int previousIndex = [[game answers] indexOfObject:[NSNumber numberWithInt:[game heldAnswer]]];
-            id previousButton = [answerButtons objectAtIndex:previousIndex];
-            [previousButton setTitleColor:[UIColor colorWithWhite:0 alpha:1] forState:UIControlStateNormal];
-        }
-        // This answer has been 'picked up'
-        [game setHeldAnswer:[answer intValue]];
-    }
-}
-
-- (IBAction)touchTarget:(id)sender
-{
-    UIButton *targetButton = (UIButton *)sender;
-    int position = [targetButtons indexOfObject:targetButton];
-    int answer = [game heldAnswer];
-    // Check if there is an answer in-hand
-    if(answer > 0){
-        int answerIndex = [[game answers] indexOfObject:[NSNumber numberWithInt:answer]];
-        id answerButton = [answerButtons objectAtIndex:answerIndex];
-        // Check if this target has already been used
-        if([[game userAnswers] objectAtIndex:position] == (id)[NSNull null]){
-            [[game userAnswers] replaceObjectAtIndex:position withObject:[NSNumber numberWithInt:answer]];
-            [game setHeldAnswer:0]; // Reset held answer
-            // Remove answer from answerButton
-            [answerButton setTitleColor:[UIColor colorWithWhite:0 alpha:1] forState:UIControlStateNormal];
-            [answerButton setTitle:[NSString stringWithFormat:@""]  forState: UIControlStateNormal];
-            [answerButton setBackgroundImage:nil forState: UIControlStateNormal];
-            // Appear to move the answer onto the target
-            [targetButton setTitle:[NSString stringWithFormat:@"%i", answer]  forState: UIControlStateNormal];
-            [targetButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-            [targetButton setBackgroundImage:[UIImage imageNamed:@"AnswerBox.png"] forState: UIControlStateNormal];
-            
-        }
-    }else{
-        // User isn't dropping anything on the target. 
-        // Check if there is an answer in this targetButton
-        id userAnswer = [[game userAnswers] objectAtIndex:position];
-        if(userAnswer != [NSNull null]){
-            // Assume they want to revert it to blank 
-            int originalPosition = [[game answers] indexOfObject:userAnswer];
-            id originalButton = [answerButtons objectAtIndex:originalPosition];
-            // Remove from the userAnswers array
-            [[game userAnswers] replaceObjectAtIndex:position withObject:[NSNull null]];
-            // Reset button states
-            [originalButton setTitle:[NSString stringWithFormat:@"%i", [userAnswer intValue]]  forState: UIControlStateNormal];
-            [originalButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-            [originalButton setBackgroundImage:[UIImage imageNamed:@"AnswerBox.png"] forState: UIControlStateNormal];
-            [targetButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-            [targetButton setTitle:[NSString stringWithFormat:@""]  forState: UIControlStateNormal];
-            [targetButton setBackgroundImage:[UIImage imageNamed:@"TransparentAnswerBox.png"] forState: UIControlStateNormal];
-        }
-        
-    }
-}
-
-- (IBAction)checkAnswers:(id)sender{
-    
-    int total = [game calculateScore];
-    
-    for(int i=0;i<[game numQuestions];i++) {
+        // Reset target button style
         id targetButton = [targetButtons objectAtIndex:i];
-        id userAnswer = [[game userAnswers] objectAtIndex:i];
-        id question = [[game questions] objectAtIndex:i];
-        if(userAnswer != (id)[NSNull null] && level * [question intValue] == [userAnswer intValue]){
-            [targetButton setTitleColor:[UIColor greenColor] forState:UIControlStateNormal];
-        }else{
-            [targetButton setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
-        }
+        [targetButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+        [targetButton setTitle:[NSString stringWithFormat:@""]  forState: UIControlStateNormal];
+        [targetButton setBackgroundImage:[UIImage imageNamed:@"TransparentAnswerBox.png"] forState: UIControlStateNormal];
+        
+        // Set the answerButton text and style
+        id answerButton = [answerButtons objectAtIndex:i];
+        id answer = [[game answers] objectAtIndex:i];
+        [answerButton setTitle:[NSString stringWithFormat:@"%i", [answer intValue]] forState: UIControlStateNormal];
+        [answerButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+        [answerButton setBackgroundImage:[UIImage imageNamed:@"AnswerBox.png"] forState: UIControlStateNormal];
     }
-    
-    id alertMessage = [NSString stringWithFormat:@"You scored %i out of %i", total, [game numQuestions]];
-    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Well Done!"
-                                                    message:alertMessage
-                                                   delegate:nil
-                                          cancelButtonTitle:@"OK"
-                                          otherButtonTitles:nil];
-    [alert show];
-    
-    // Call initialiseAction to reset game
-    //[self initialiseAction:nil];
 }
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+- (void)initElementArrays{
+    // Order questionLabels array by tag
+    self.questionLabels = [self.questionLabels sortedArrayUsingComparator:^NSComparisonResult(id label1, id label2) {
+        if ([label1 tag] < [label2 tag]) return NSOrderedAscending;
+        else if ([label1 tag] > [label2 tag]) return NSOrderedDescending;
+        else return NSOrderedSame;
+    }];
+    
+    
+    // Order targetButtons array by tag
+    self.targetButtons = [self.targetButtons sortedArrayUsingComparator:^NSComparisonResult(id label1, id label2) {
+        if ([label1 tag] < [label2 tag]) return NSOrderedAscending;
+        else if ([label1 tag] > [label2 tag]) return NSOrderedDescending;
+        else return NSOrderedSame;
+    }];
+    
+    // Order answerButtons array by tag
+    self.answerButtons = [self.answerButtons sortedArrayUsingComparator:^NSComparisonResult(id label1, id label2) {
+        if ([label1 tag] < [label2 tag]) return NSOrderedAscending;
+        else if ([label1 tag] > [label2 tag]) return NSOrderedDescending;
+        else return NSOrderedSame;
+    }];
 }
 
 - (void)didReceiveMemoryWarning
